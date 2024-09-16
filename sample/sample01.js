2088b0b6e9dbe1aaab2d7dc447d29da03e6b1b8b
root <root@kono3>
2024-09-16 19:24:06 +0900

    todoアプリ完成例を配置

---
+++ sample/sample01.js
@@ -0,0 +1,13 @@
+// const iniName = ["inoue", "yoshikawa", "tanaka","none"]
+
+// const ans = iniName.filter((iname => {
+//     if (iname != "none"){
+//         console.log(`${iname}さん`)
+//     }
+//     else{
+//         console.log("hoge")
+//     }
+// }))
+
+
+
